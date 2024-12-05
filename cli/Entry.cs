3d215c31c2f15d2e958d6a8159004a3752396e9b
Lucas Pimentel <lucaspimentel@gmail.com>
2024-12-05 15:49:30 -0500

    initial commit of cli tool

---
+++ cli/Entry.cs
@@ -0,0 +1,13 @@
+﻿namespace FindDuplicateChromeTabs;
+
+public class Entry
+{
+    public required string Id { get; set; }
+    public string Description { get; set; }
+    public string DevToolsFrontendUrl { get; set; }
+    public string FaviconUrl { get; set; }
+    public string Title { get; set; }
+    public required string Type { get; set; }
+    public required string Url { get; set; }
+    public string WebSocketDebuggerUrl { get; set; }
+}
---
+++ cli/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Json;
+using FindDuplicateChromeTabs;
+
+Console.WriteLine("Hello, World!");
+
+if (args.Length == 0 || !int.TryParse(args[0], out var port))
+{
+    port = 9222;
+}
+
+var client = new HttpClient();
+var tags = await client.GetFromJsonAsync<List<Entry>>($"http://localhost:{port}/json");
+
+var duplicateTabs = tags!.GroupBy(e => e.Url)
+                         .Where(g => g.Count() > 1)
+                         .Select(g => g.Key);
+
+foreach (var url in duplicateTabs)
+{
+    Console.WriteLine(url);
+}
